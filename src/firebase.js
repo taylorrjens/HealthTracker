be8e5ef71d6086297e7fbf34c8a47ffbdbf7835c
taylorrjens <54914754+taylorrjens@users.noreply.github.com>
2019-11-07 17:33:02 -0700

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const config = {
+  apiKey: "AIzaSyD22ffuYjuvTPm1u33Y64qbmgmE-fi8m9o",
+  authDomain: "authentication2-a2c4b.firebaseapp.com",
+  databaseURL: "https://authentication2-a2c4b.firebaseio.com",
+  projectId: "authentication2-a2c4b",
+  storageBucket: "",
+  messagingSenderId: "365655359740",
+  appId: "1:365655359740:web:b34c6c42488fcdb0cdd779"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+export const db = firebase.firestore();
